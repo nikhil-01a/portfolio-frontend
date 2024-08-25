879ac5ac2be14653e8b9c30e78cab6cff0058589
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-08-25 12:41:57 -0400

    Header styling for portfolio page

--- src/components/portfolio/Header.js
+++ src/components/portfolio/Header.js
@@ -2,8 +2,8 @@ import { SocialIcon } from "react-social-icons";
 
 export default function Header() {
   return (
-    <header>
-      <div>
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto p-8 z-20 xl:items-center">
+      <div className="flex flex-row items-center">
         <SocialIcon
           url="https://www.instagram.com/alexandrab_design/"
           bgColor="transparent"
@@ -14,12 +14,12 @@ export default function Header() {
           bgColor="transparent"
           fgColor="gray"
         />
-        <SocialIcon
-          network="email"
-          bgColor="transparent"
-          fgColor="gray"
-          className="cursor-pointer"
-        />
+      </div>
+      <div className="flex flex-row items-center cursor-pointer text-gray-300">
+        <SocialIcon network="email" bgColor="transparent" fgColor="gray" />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
       </div>
     </header>
   );
