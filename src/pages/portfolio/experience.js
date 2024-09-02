4219fcbbee0e8931d6a2ada417a4c66074769088
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-02 12:31:29 -0400

    1. Using header component on all portfolio pages and using motion only on initial page, 2. Refactored Header component's JSX, 3.Changed Prettier's line character count to 120

--- src/pages/portfolio/experience.js
+++ src/pages/portfolio/experience.js
@@ -1,6 +1,9 @@
+import Header from "../../components/portfolio/Header";
+
 export default function Experience() {
   return (
     <div>
+      <Header />
       <h1>This is an Experience Page</h1>
     </div>
   );
--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -1,6 +1,9 @@
+import Header from "../../components/portfolio/Header";
+
 export default function Homepage() {
   return (
     <div>
+      <Header homepage={true} />
       {/* Header: Done and added above all routes in App.js */}
 
       {/* Hero */}
