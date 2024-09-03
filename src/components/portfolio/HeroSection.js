7fa7cb8662f8cf091c44119a8e50539bb7fef8f7
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-03 00:41:40 -0400

    Hero Section Start

---
+++ src/components/portfolio/HeroSection.js
@@ -0,0 +1,30 @@
+export default function HeroSection() {
+  return (
+    <div className="relative items-center flex flex-col justify-start">
+      <h1
+        style={{
+          fontFamily: 'Inter Tight, sans-serif',
+          fontSize: '52px',
+          fontWeight: 600,
+          textTransform: 'uppercase',
+          letterSpacing: '.04em',
+          lineHeight: '50px',
+        }}
+      >
+        Alexandra
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Inter Tight, sans-serif',
+          fontSize: '52px',
+          fontWeight: 600,
+          textTransform: 'uppercase',
+          letterSpacing: '.04em',
+          lineHeight: '50px',
+        }}
+      >
+        Biehle
+      </h1>
+    </div>
+  )
+}
--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -1,12 +1,14 @@
 import Header from '../../components/portfolio/Header'
+import HeroSection from '../../components/portfolio/HeroSection'
 
 export default function Homepage() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen">
-      <Header homepage={true} />
       {/* Header: Done and added above all routes in App.js */}
+      <Header homepage={true} />
 
       {/* Hero */}
+      <HeroSection />
       {/* About */}
       {/* Experience */}
       {/* Skills */}
