f3dd11f6927446a8f20e49fc5adc2c263445ac5d
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-03 01:46:41 -0400

    Hero Section

--- src/components/portfolio/HeroSection.js
+++ src/components/portfolio/HeroSection.js
@@ -1,30 +1,33 @@
 export default function HeroSection() {
   return (
-    <div className="relative items-center flex flex-col justify-start">
-      <h1
-        style={{
-          fontFamily: 'Inter Tight, sans-serif',
-          fontSize: '52px',
-          fontWeight: 600,
-          textTransform: 'uppercase',
-          letterSpacing: '.04em',
-          lineHeight: '50px',
-        }}
-      >
-        Alexandra
-      </h1>
-      <h1
-        style={{
-          fontFamily: 'Inter Tight, sans-serif',
-          fontSize: '52px',
-          fontWeight: 600,
-          textTransform: 'uppercase',
-          letterSpacing: '.04em',
-          lineHeight: '50px',
-        }}
-      >
-        Biehle
-      </h1>
+    <div className="flex flex-wrap m-52 ml-80">
+      <div className="w-full lg:w-1/2">
+        <div className=" flex flex-col items-center lg:items-start"></div>
+        <h1
+          className="text:6xl mt-9 lg:text-8xl"
+          style={{
+            fontFamily: 'Inter Tight, sans-serif',
+            fontWeight: 600,
+            textTransform: 'uppercase',
+            letterSpacing: '.04em',
+            lineHeight: '50px',
+          }}
+        >
+          Alexandra
+        </h1>
+        <h1
+          className="text:6xl mt-9 lg:text-8xl"
+          style={{
+            fontFamily: 'Inter Tight, sans-serif',
+            fontWeight: 600,
+            textTransform: 'uppercase',
+            letterSpacing: '.04em',
+            lineHeight: '50px',
+          }}
+        >
+          Biehle
+        </h1>
+      </div>
     </div>
   )
 }
