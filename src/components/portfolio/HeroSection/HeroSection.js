2f9d555e3ce5022423ebe3c24b2f947ab73b72c8
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-08 14:41:00 -0400

    Preliminary animations

--- src/components/portfolio/HeroSection/HeroSection.js
+++ src/components/portfolio/HeroSection/HeroSection.js
@@ -1,5 +1,6 @@
 import profilePic from '../../../assets/IMG_6768.jpg'
 import './HeroSection.css'
+import { motion } from 'framer-motion'
 
 export default function HeroSection() {
   return (
@@ -7,31 +8,52 @@ export default function HeroSection() {
       <div className="flex flex-col 2xl:flex-row">
         <div className="flex-1 flex flex-col justify-center 2xl:pr-20">
           <div className="flex flex-col items-center">
-            <h1 className="hero-text mt-4 mb-8 text-3xl leading-[0rem] xs:text-5xl xs:leading-[1rem] lg:text-6xl lg:leading-[1.5rem]">Alexandra</h1>
-            <h1 className="hero-text mb-12 xs:mb-16 text-3xl leading-[0rem] xs:text-5xl xs:leading-[1rem] lg:text-6xl lg:leading-[1.5rem]">Biehle</h1>
+            <h1 className="hero-text mt-4 mb-8 text-3xl leading-[0rem] xs:text-5xl xs:leading-[1rem] lg:text-6xl lg:leading-[1.5rem]"> {AnimatedText('Alexandra', 0.05, 0.1)} </h1>
+            <h1 className="hero-text mb-12 xs:mb-16 text-3xl leading-[0rem] xs:text-5xl xs:leading-[1rem] lg:text-6xl lg:leading-[1.5rem]">{AnimatedText('Biehle', 0.05, 0.2)}</h1>
           </div>
           <div className="flex flex-col items-start 2xl:items-start xs:ml-12 xl:ml-10">
             <div>
               <div className="flex flex-col items-start custom-spacing">
-                <h1 className="hero-text text-base xs:text-xl lg:text-2xl">- Graphic Designer</h1>
-                <h1 className="hero-text mb-8 text-base xs:text-xl lg:text-2xl xs:mb-16">- Outreach Coordinator</h1>
+                <h1 className="hero-text text-base xs:text-xl lg:text-2xl">{AnimatedText('- Graphic Designer', 0.03)}</h1>
+                <h1 className="hero-text mb-8 text-base xs:text-xl lg:text-2xl xs:mb-16">{AnimatedText('- Outreach Coordinator', 0.03)}</h1>
               </div>
             </div>
           </div>
           <div className="flex flex-col items-start">
             <div className="flex flex-row mb-4 items-center justify-between w-full custom-spacing">
               <div>
-                <h1 className="hero-text text-xs xs:text-lg">Currently Designing At</h1>
-                <h1 className="hero-text text-xs xs:text-lg">Marshal Retail Group</h1>
+                <h1 className="hero-text text-xs xs:text-lg">{AnimatedText('Currently Designing At', 0.03)}</h1>
+                <h1 className="hero-text text-xs xs:text-lg">{AnimatedText('Marshal Retail Group', 0.03)}</h1>
               </div>
-              <h1 className="hero-text text-xs xs:text-lg 2xl:mr-5">(2023 - PRESENT)</h1>
+              <h1 className="hero-text text-xs xs:text-lg 2xl:mr-5">{AnimatedText('(2023 - PRESENT)', 0.03, 0.5)}</h1>
             </div>
           </div>
         </div>
         <div className="flex-1 pt-5 xl2:pl-5">
-          <img src={profilePic} alt="Alexandra Biehle" className="rounded-3xl" />
+          <motion.img className="rounded-3xl" initial={{ opacity: 0, x: -500 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1 }} src={profilePic} alt="Alexandra Biehle" />
         </div>
       </div>
     </div>
+  )
+}
+
+export const AnimatedText = (text, speed, delay) => {
+  return (
+    <motion.span initial="hidden" animate="visible" transition={{ staggerChildren: speed - 0.01, delayChildren: delay }}>
+      {text.split('').map((char) => (
+        <motion.span
+          variants={{
+            hidden: {
+              opacity: 0,
+            },
+            visible: {
+              opacity: 1,
+            },
+          }}
+        >
+          {char}
+        </motion.span>
+      ))}
+    </motion.span>
   )
 }
