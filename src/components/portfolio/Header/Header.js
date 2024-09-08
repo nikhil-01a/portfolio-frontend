287e08246e4b659bf720502b68c1060d8106757b
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-08 02:31:19 -0400

    Header moved in a new folder

---
+++ src/components/portfolio/Header/Header.js
@@ -0,0 +1,47 @@
+import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
+
+export default function Header({ homepage = false }) {
+  const DynamicDiv = homepage ? motion.div : 'div'
+  const animationPropsLeft = getHeaderAnimation(homepage, 'left')
+  const animationPropsRight = getHeaderAnimation(homepage, 'right')
+  const getInstagramAttributes = getSocialIconsAttributes('url', 'https://www.instagram.com/alexandrab_design/')
+  const getLinkedInAttributes = getSocialIconsAttributes('url', 'https://www.linkedin.com/in/alexandra-b-a8a27b146/')
+  const getEmailAttributes = getSocialIconsAttributes('network', 'email')
+
+  return (
+    <header className="sticky top-0 flex items-start justify-between max-w-full overflow-x-hidden mx-auto p-8 z-20 xl:items-center">
+      <DynamicDiv {...animationPropsLeft} className="flex flex-row items-center">
+        <SocialIcon {...getInstagramAttributes} />
+        <SocialIcon {...getLinkedInAttributes} />
+      </DynamicDiv>
+      <DynamicDiv {...animationPropsRight} className="flex flex-row items-center cursor-pointer text-white">
+        <SocialIcon {...getEmailAttributes} />
+        <p
+          className="uppercase hidden md:inline-flex text-white"
+          style={{ fontFamily: 'Inter Tight, sans-serif', fontSize: '16px', fontWeight: 600, letterSpacing: '.08em' }}
+        >
+          Get In Touch
+        </p>
+      </DynamicDiv>
+    </header>
+  )
+}
+
+const getHeaderAnimation = (homepage, direction) => {
+  if (!homepage) return {}
+  const initialX = direction === 'left' ? -500 : 500
+  return {
+    initial: { x: initialX, opacity: 1, scale: 0.5 },
+    animate: { x: 0, opacity: 1, scale: 1 },
+    transition: { duration: 0.5 },
+  }
+}
+
+const getSocialIconsAttributes = (mainProperty, mainPropertyValue) => {
+  return {
+    [mainProperty]: mainPropertyValue,
+    bgColor: 'transparent',
+    fgColor: 'white',
+  }
+}
--- src/components/portfolio/HeroSection/HeroSection.js
+++ src/components/portfolio/HeroSection/HeroSection.js
@@ -5,7 +5,7 @@ export default function HeroSection() {
   return (
     <div className="ml-8 mr-8 sm:ml-20 sm:mr-20 md:ml-40 md:mr-40 lg:ml-60 lg:mr-60 xl:ml-96 xl:mr-96 2xl:ml-72">
       <div className="flex flex-col 2xl:flex-row">
-        <div className="flex-1 flex flex-col justify-center xl2:pr-5">
+        <div className="flex-1 flex flex-col justify-center 2xl:pr-20">
           <div className="flex flex-col items-center">
             <h1 className="hero-text mt-4 mb-8 text-3xl leading-[0rem] xs:text-5xl xs:leading-[1rem] lg:text-6xl lg:leading-[1.5rem]">Alexandra</h1>
             <h1 className="hero-text mb-12 xs:mb-16 text-3xl leading-[0rem] xs:text-5xl xs:leading-[1rem] lg:text-6xl lg:leading-[1.5rem]">Biehle</h1>
