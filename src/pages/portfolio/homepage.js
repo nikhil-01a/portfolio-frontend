4af5288ee74beff9a506e9dbab88f83cde442ec5
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-09 01:08:55 -0400

    re-arrange components

--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -4,11 +4,8 @@ import HeroSection from '../../components/portfolio/HeroSection/HeroSection'
 export default function Homepage() {
   return (
     <div className=" text-white ">
-      {/* Header: Done and added above all routes in App.js */}
       <Header />
-
       <div className="flex flex-col items-center">
-        {/* Hero */}
         <HeroSection />
         {/* About */}
         {/* Experience */}
