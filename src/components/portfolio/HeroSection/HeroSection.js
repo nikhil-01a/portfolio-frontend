096016dd43fe5b2ef4bc7fd0d63f1ea168683ed5
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-14 00:00:28 -0400

    Image fade in and out

--- src/components/portfolio/HeroSection/HeroSection.js
+++ src/components/portfolio/HeroSection/HeroSection.js
@@ -3,6 +3,9 @@ import './HeroSection.css'
 import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function HeroSection() {
+  const { scrollY } = useScroll()
+  const opacity = useTransform(scrollY, [0, 100, 450, 850], [0.4, 1, 1, 0])
+
   return (
     <div className="relative w-full flex flex-col justify-center sm:pr-20 md:pr-40 lg:pr-60 xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-96">
       <TopText />
@@ -10,11 +13,12 @@ export default function HeroSection() {
         <div className="lg:pl-16 lg:pr-16 1xl:pl-24 1xl:pr-24 2xl:pl-36 2xl:pr-36">
           <motion.img
             className="rounded-3xl "
-            initial={{ opacity: 0, x: 0, y: 0 }}
-            animate={{ opacity: 1, x: 0, y: 20 }}
+            initial={{ opacity: 0, y: 0 }}
+            animate={{ opacity: 0.4, y: 20 }}
             transition={{ duration: 0.5, delay: 0.2 }}
             src={profilePic}
             alt="Alexandra Biehle"
+            style={{ opacity }}
           />
         </div>
       </div>
