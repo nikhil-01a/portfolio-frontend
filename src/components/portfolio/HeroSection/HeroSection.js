02202250d5aea6ca2da0d530c7aed3988c90e72b
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-14 18:15:50 -0400

    final sizes

--- src/components/portfolio/HeroSection/HeroSection.js
+++ src/components/portfolio/HeroSection/HeroSection.js
@@ -4,10 +4,10 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function HeroSection() {
   const { scrollY } = useScroll()
-  const opacity = useTransform(scrollY, [0, 180, 450, 850], [0.4, 1, 1, 0])
+  const opacity = useTransform(scrollY, [0, 250, 450, 850], [0.4, 1, 1, 0])
 
   return (
-    <div className="relative w-full flex flex-col justify-center pt-16 xs:pt-0 sm:pr-20 md:pr-40 lg:pr-60 xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-96">
+    <div className="relative w-full flex flex-col justify-center pt-16 xs:pt-16 sm:pr-20 md:pr-40 lg:pr-60 xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-96">
       <TopText />
       <div className="flex-1 z-10 pl-12 pr-12 xs:pt-2 flex justify-center">
         <div className="lg:pl-16 lg:pr-16 1xl:pl-24 1xl:pr-24 2xl:pl-36 2xl:pr-36">
@@ -28,15 +28,15 @@ export default function HeroSection() {
 
 const TopText = () => {
   const { scrollY } = useScroll()
-  const opacity = useTransform(scrollY, [0, 200], [1, 0])
+  const opacity = useTransform(scrollY, [0, 250], [1, 0])
 
   return (
     <motion.div className="flex-1 sticky top-20 xs:top-12 pl-12 pr-12 md:p-0" style={{ opacity }}>
       <div className="flex flex-col items-center">
-        <h1 className="hero-text xs:pt-8 pb-8 text-[2.9rem] leading-[0.6rem] xs:text-5xl xs:leading-[1rem] lg:text-[3.25rem] 1xl:text-[5rem] 1xl:leading-[2.2rem] 2xl:text-[5.5rem] 2xl:leading-[2.8rem]">
+        <h1 className="hero-text xs:pt-8 pb-8 text-[2.9rem] leading-[0.6rem] xs:text-5xl xs:leading-[1rem] lg:text-[3.25rem] 1xl:text-[5.5rem] 1xl:leading-[2.6rem] 2xl:text-[6rem] 2xl:leading-[3rem]">
           {AnimatedText('Alexandra', 0.05, 0.1)}
         </h1>
-        <h1 className="hero-text pb-12 text-[2.9rem] leading-[0.6rem] xs:text-5xl xs:leading-[1rem] lg:text-[3.25rem] 1xl:text-[5rem] 1xl:leading-[2.2rem] 2xl:text-[5.5rem] 2xl:leading-[2.8rem]">
+        <h1 className="hero-text pb-12 text-[2.9rem] leading-[0.6rem] xs:text-5xl xs:leading-[1rem] lg:text-[3.25rem] 1xl:text-[5.5rem] 1xl:leading-[2.6rem] 2xl:text-[6rem] 2xl:leading-[3rem]">
           {AnimatedText('Biehle', 0.05, 0.2)}
         </h1>
       </div>
