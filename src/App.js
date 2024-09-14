f0fa15f7e884b72f49d51d79b90eacaae2229914
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-14 18:47:25 -0400

    removed about page and routing from App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/portfolio/homepage";
-import Experience from "./pages/portfolio/experience";
-import About from "./pages/portfolio/about";
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Homepage from './pages/portfolio/homepage'
+import Experience from './pages/portfolio/experience'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
         <Route path="/experience" element={<Experience />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
