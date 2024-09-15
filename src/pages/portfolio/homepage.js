d877df378961f6c2cc83fb1ec1f8b3a202506d41
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-15 03:14:06 -0400

    Added lenis scroll

--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -1,17 +1,20 @@
 import Header from '../../components/portfolio/Header/Header'
 import HeroSection from '../../components/portfolio/HeroSection/HeroSection'
+import ReactLenis from 'lenis/dist/lenis-react'
 
 export default function Homepage() {
   return (
     <div className=" text-white ">
-      <Header />
-      <HeroSection />
-      <div className="h-screen"></div>
-      {/* About */}
-      {/* Experience */}
-      {/* Projects */}
-      {/* Skills */}
-      {/* Contact Me*/}
+      <ReactLenis root>
+        <Header />
+        <HeroSection />
+        <div className="h-screen"></div>
+        {/* About */}
+        {/* Experience */}
+        {/* Projects */}
+        {/* Skills */}
+        {/* Contact Me*/}
+      </ReactLenis>
     </div>
   )
 }
