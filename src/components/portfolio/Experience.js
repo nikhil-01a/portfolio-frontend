d7f9d6a0d2e32593f8699ea9367e753e72b749a3
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-15 18:10:15 -0400

    experience progress

--- src/components/portfolio/Experience.js
+++ src/components/portfolio/Experience.js
@@ -1,13 +1,43 @@
 import { motion, useScroll, useTransform } from 'framer-motion'
 export default function Experience() {
   const { scrollY } = useScroll()
-  const opacity = useTransform(scrollY, [2000, 2200], [0, 1])
+  const opacity = useTransform(scrollY, [1900, 2100], [0, 1])
+  const experienceOpacity = useTransform(scrollY, [1950, 2170, 2200, 2215], [0, 0.2, 0.2, 1])
+  const experienceScale = useTransform(scrollY, [1950, 2170, 2200, 2215], [0.98, 0.98, 0.98, 1])
 
   return (
-    <div className=" flex flex-col items-start pt-80 sm:pr-20 md:pr-40 lg:pr-60 xl:pr-96 sm:pl-20 md:pl-20 lg:pl-40 xl:pl-52 hero-text">
-      <motion.div style={{ opacity }}>
+    <div className=" flex flex-col pt-80 sm:pr-20 md:pr-40 lg:pr-60 xl:pr-48 sm:pl-20 md:pl-20 lg:pl-40 xl:pl-48 hero-text">
+      <motion.div className="flex-col items-start" style={{ opacity }}>
         <motion.h1 className="text-[3.5rem] leading-[3rem]">RELEVANT</motion.h1>
         <motion.h1 className="text-[3.5rem] leading-[3rem]">EXPERIENCE</motion.h1>
+      </motion.div>
+      <motion.div className="flex pl-20 pt-8 flex-col justify-center hero-text2" style={{ opacity: experienceOpacity, scale: experienceScale }}>
+        <div>
+          <h1 className=" text-[1.5rem] leading-[3rem]">First Experience</h1>
+          <h1 className=" text-[0.75rem] hero-text">First Experience</h1>
+        </div>
+        <div class="h-px bg-gray-500 my-4"></div>
+        <div>
+          <h1 className="text-[1.5rem] leading-[3rem]">First Experience</h1>
+          First Experience
+        </div>
+        <div class="h-px bg-gray-500 my-4"></div>
+        <div>
+          <h1 className="text-[1.5rem] leading-[3rem]">First Experience</h1>
+          First Experience
+        </div>
+        <div class="h-px bg-gray-500 my-4"></div>
+
+        <div>
+          <h1 className="text-[1.5rem] leading-[3rem]">First Experience</h1>
+          First Experience
+        </div>
+        <div class="h-px bg-gray-500 my-4"></div>
+
+        <div>
+          <h1 className="text-[1.5rem] leading-[3rem]">First Experience</h1>
+          First Experience
+        </div>
       </motion.div>
     </div>
   )
