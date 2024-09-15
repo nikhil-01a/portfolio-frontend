492ed62c72a158d015972ae642e8424d2d1d0826
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-15 15:18:32 -0400

    Added experience section and title

--- src/components/portfolio/Experience.js
+++ src/components/portfolio/Experience.js
@@ -1,9 +1,12 @@
-function Experience() {
-  return (
-    <div>
-      <h1>This is the Experience Section</h1>
-    </div>
-  );
-}
+import { motion, useScroll, useTransform } from 'framer-motion'
+export default function Experience() {
+  const { scrollY } = useScroll()
+  const opacity = useTransform(scrollY, [2000, 2200], [0, 1])
 
-export default Experience;
+  return (
+    <motion.div className=" flex flex-col items-start pt-80 sm:pr-20 md:pr-40 lg:pr-60 xl:pr-96 sm:pl-20 md:pl-20 lg:pl-40 xl:pl-44 hero-text" style={{ opacity }}>
+      <motion.h1 className="text-[3.5rem] leading-[3rem]">RELEVANT</motion.h1>
+      <motion.h1 className="text-[3.5rem] leading-[3rem]">EXPERIENCE</motion.h1>
+    </motion.div>
+  )
+}
--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -2,6 +2,7 @@ import Header from '../../components/portfolio/Header/Header'
 import HeroSection from '../../components/portfolio/HeroSection/HeroSection'
 import About from '../../components/portfolio/About/About'
 import ReactLenis from 'lenis/dist/lenis-react'
+import Experience from '../../components/portfolio/Experience'
 
 export default function Homepage() {
   return (
@@ -10,6 +11,7 @@ export default function Homepage() {
         <Header />
         <HeroSection />
         <About />
+        <Experience />
         <div className="h-screen"></div>
         {/* About */}
         {/* Experience */}
