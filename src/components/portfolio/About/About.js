2364ed985b7a80357d17a690a7d944969dca64f3
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-15 13:41:04 -0400

    text spacing

--- src/components/portfolio/About/About.js
+++ src/components/portfolio/About/About.js
@@ -2,11 +2,11 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function About() {
   const { scrollY } = useScroll()
-  const x = useTransform(scrollY, [600, 1000], [-1000, 0])
-  const scale = useTransform(scrollY, [600, 1000, 1150], [0.9, 0.9, 1])
-  const opacity = useTransform(scrollY, [600, 900, 1200, 1380], [0.5, 1, 1, 0])
-  const textOpacity = useTransform(scrollY, [1000, 1100, 1200, 1380], [0, 1, 1, 0])
-  const tagLineOpacity = useTransform(scrollY, [1300, 1400, 1500, 1700], [0, 1, 1, 0])
+  const x = useTransform(scrollY, [1000, 1100], [-1000, 0])
+  const scale = useTransform(scrollY, [600, 1000, 1150], [0.5, 0.5, 1])
+  const opacity = useTransform(scrollY, [1000, 1100, 1200, 1450], [0, 1, 1, 0])
+  const textOpacity = useTransform(scrollY, [1000, 1100, 1200, 1450], [0, 1, 1, 0])
+  const tagLineOpacity = useTransform(scrollY, [1450, 1550, 1650, 1850], [0, 1, 1, 0])
 
   return (
     <div>
@@ -20,7 +20,7 @@ export default function About() {
           <br />I value attention to detail, composition, and color, always approaching each project with meticulous care. My dream is to collaborate with others on projects that make an impact,
           inspire, and bring joy. From childhood, I’ve been drawn to art and design - whether crafting, writing stories, or creating virtual worlds.
         </motion.p>
-        <motion.h1 className="pt-20 text-[2rem]" style={{ scale, opacity: tagLineOpacity }}>
+        <motion.h1 className="pt-60 text-[2rem]" style={{ scale, opacity: tagLineOpacity }}>
           Let’s create something meaningful together
         </motion.h1>
       </div>
