af44ea9beb9de2b23011b9230904b589e0f552fe
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-15 18:41:56 -0400

    Optimized for mac 13 inch

--- src/components/portfolio/HeroSection/HeroSection.js
+++ src/components/portfolio/HeroSection/HeroSection.js
@@ -4,7 +4,7 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function HeroSection() {
   return (
-    <div className="relative w-full flex flex-col justify-center pt-32 xs:pt-16 sm:pr-20 md:pr-40 lg:pr-60 xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-96">
+    <div className="relative w-full flex flex-col justify-center pt-32 xs:pt-16 sm:pr-20 md:pr-40 lg:pr-60 xl:pr-80 1xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-80 1xl:pl-96">
       <TopText />
       <BottomImage />
     </div>
@@ -18,10 +18,10 @@ const TopText = () => {
   return (
     <motion.div className="flex-1 sticky top-28 xs:top-12 pl-12 pr-12 md:p-0" style={{ opacity, scale }}>
       <div className="flex flex-col items-center">
-        <h1 className="hero-text xs:pt-8 pb-8 text-[2.9rem] leading-[0.6rem] xs:text-[3.5rem] xs:leading-[2rem]  lg:leading-[1rem] 1xl:text-[7.3rem] 1xl:leading-[4rem] 2xl:text-[8rem] 2xl:leading-[4.55rem]">
+        <h1 className="hero-text xs:pt-8 pb-8 text-[2.9rem] leading-[0.6rem] xs:text-[3.5rem] xs:leading-[2rem]  lg:leading-[1rem] xl:text-[6.2rem] xl:leading-[3rem] 1xl:text-[7.3rem] 1xl:leading-[4rem] 2xl:text-[8rem] 2xl:leading-[4.55rem]">
           {AnimatedText('Alexandra', 0.05, 0.1)}
         </h1>
-        <h1 className="hero-text pb-16 text-[2.9rem] leading-[0.6rem] xs:text-[3.5rem] xs:leading-[2rem] lg:leading-[1rem] 1xl:text-[7.3rem] 1xl:leading-[4rem] 2xl:text-[8rem] 2xl:leading-[4.55rem]">
+        <h1 className="hero-text pb-16 text-[2.9rem] leading-[0.6rem] xs:text-[3.5rem] xs:leading-[2rem] lg:leading-[1rem] xl:text-[6.2rem] xl:leading-[3rem] 1xl:text-[7.3rem] 1xl:leading-[4rem] 2xl:text-[8rem] 2xl:leading-[4.55rem]">
           {AnimatedText('Biehle', 0.05, 0.2)}
         </h1>
       </div>
