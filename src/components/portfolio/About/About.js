0ca8d7eff21114d286ee70d9839a237b068781ad
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-15 05:44:50 -0400

    About me heading

--- src/components/portfolio/About/About.js
+++ src/components/portfolio/About/About.js
@@ -3,8 +3,8 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 export default function About() {
   const { scrollY } = useScroll()
   const x = useTransform(scrollY, [600, 1000], [-1000, 0])
-  const scale = useTransform(scrollY, [600, 1100, 1150], [0.7, 0.7, 1])
-  const opacity = useTransform(scrollY, [600, 900, 1200, 1250], [0.5, 1, 1, 0])
+  const scale = useTransform(scrollY, [600, 1100, 1150], [0.9, 0.9, 1])
+  const opacity = useTransform(scrollY, [600, 900, 1200, 1350], [0.5, 1, 1, 0])
   return (
     <div>
       <div className="overflow-x-hidden sticky top-[2500px] flex justify-center pt-28 sm:pr-20 md:pr-40 lg:pr-60 xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-96 hero-text ">
