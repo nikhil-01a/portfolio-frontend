48d6eb4a248d1b1abbd551ab975bddc850390715
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-16 21:06:18 -0400

    Moving folders/files

--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -2,7 +2,7 @@ import Header from '../../components/portfolio/Header/Header'
 import HeroSection from '../../components/portfolio/HeroSection/HeroSection'
 import About from '../../components/portfolio/About/About'
 import ReactLenis from 'lenis/dist/lenis-react'
-import Experience from '../../components/portfolio/Experience'
+import Experience from '../../components/portfolio/Experience/Experience'
 
 export default function Homepage() {
   return (
