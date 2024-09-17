70d1918e467ff002340b1446e23b6a6e72c71bc9
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-16 23:54:04 -0400

    fixed for image

--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -13,13 +13,13 @@ export default function Homepage() {
       <ReactLenis root>
         <Header />
         <motion.div
-          className="sticky top-0 sm:pr-[8rem] sm:pl-[8rem] md:pr-[13rem] md:pl-[13rem] lg:pr-[18rem] lg:pl-[18rem] xl:pr-[23rem] xl:pl-[23rem] 2xl:pr-[27rem] 2xl:pl-[27rem]"
+          className="sticky top-0 pt-[5rem] sm:pr-[8rem] sm:pl-[8rem] md:pr-[13rem] md:pl-[13rem] lg:pr-[18rem] lg:pl-[18rem] xl:pr-[23rem] xl:pl-[23rem] 2xl:pr-[27rem] 2xl:pl-[27rem]"
           style={useElementOneTransform()}
         >
           <HeroText />
         </motion.div>
 
-        <div className="relative w-full flex flex-col justify-center sm:pr-20 md:pr-40 lg:pr-60 xl:pr-80 2xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-80 2xl:pl-96">
+        <div className="pt-[15rem] relative w-full flex flex-col justify-center sm:pr-20 md:pr-40 lg:pr-60 xl:pr-80 2xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-80 2xl:pl-96">
           <HeroSection {...useElementTwoTransform()} />
         </div>
         <About />
@@ -43,7 +43,8 @@ const useElementOneTransform = () => {
 
 const useElementTwoTransform = () => {
   const { scrollY } = useScroll()
-  const opacity = useTransform(scrollY, [0, 250, 550, 1150], [0.4, 1, 1, 0])
-  const scale = useTransform(scrollY, [0, 250, 650, 950], [0.8, 1, 1, 0.8])
+  console.log(scrollY.get())
+  const opacity = useTransform(scrollY, [450, 670, 1000, 1100], [0.4, 1, 1, 0.4])
+  const scale = useTransform(scrollY, [450, 670, 1000, 1100], [0.8, 1, 1, 0.8])
   return { opacity, scale }
 }
