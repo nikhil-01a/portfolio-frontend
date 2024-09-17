aa3b7b099184ebd3acf9fabdf87bcd14b20b4fb5
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-09-16 21:17:52 -0400

    ---------------------Big Change-------------------- REFACTOR

--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -3,12 +3,14 @@ import HeroSection from '../../components/portfolio/HeroSection/HeroSection'
 import About from '../../components/portfolio/About/About'
 import ReactLenis from 'lenis/dist/lenis-react'
 import Experience from '../../components/portfolio/Experience/Experience'
+import HeroText from '../../components/portfolio/HeroText/HeroText'
 
 export default function Homepage() {
   return (
     <div className=" text-white ">
       <ReactLenis root>
         <Header />
+        <HeroText />
         <HeroSection />
         <About />
         <Experience />
