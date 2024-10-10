42eebdb1772ef15fc0996cda54c26444d12f8478
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-10-09 20:54:24 -0400

    homepage and herosection refactor

--- src/components/portfolio/HeroSection/HeroSection.js
+++ src/components/portfolio/HeroSection/HeroSection.js
@@ -1,13 +1,25 @@
 import AlexLogo from '../../../assets/AlexLogo2.png'
 import './HeroSection.css'
-import { motion } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 
-export default function HeroSection({ opacity, scale }) {
+export default function HeroSection() {
   return (
-    <div className="flex-1 z-10 pl-12 pr-12 flex justify-center">
-      <div className="lg:pl-16 lg:pr-16 1xl:pl-24 1xl:pr-24 2xl:pl-36 2xl:pr-36">
-        <motion.img className="rounded-3xl " src={AlexLogo} alt="Alexandra Biehle" style={{ willChange: 'opacity, transform', opacity, scale }} />
+    <motion.div
+      className="sticky top-0 pt-[10rem] sm:pr-[8rem] sm:pl-[8rem] md:pr-[13rem] md:pl-[13rem] lg:pr-[18rem] lg:pl-[18rem] xl:pr-[23rem] xl:pl-[23rem] 2xl:pr-[27rem] 2xl:pl-[27rem]"
+      style={useElementOneTransform()}
+    >
+      <div className="flex-1 z-10 pl-12 pr-12 flex justify-center">
+        <div className="lg:pl-16 lg:pr-16 1xl:pl-24 1xl:pr-24 2xl:pl-36 2xl:pr-36">
+          <motion.img className="rounded-3xl " src={AlexLogo} alt="Alexandra Biehle" style={{ willChange: 'opacity, transform', useElementOneTransform }} />
+        </div>
       </div>
-    </div>
+    </motion.div>
   )
+}
+
+const useElementOneTransform = () => {
+  const { scrollY } = useScroll()
+  const opacity = useTransform(scrollY, [0, 80, 350], [1, 1, 0])
+  const scale = useTransform(scrollY, [0, 40, 450], [1, 1, 0.7])
+  return { opacity, scale }
 }
--- src/pages/portfolio/homepage.js
+++ src/pages/portfolio/homepage.js
@@ -17,14 +17,7 @@ export default function Homepage() {
         </motion.video>
         <div className="absolute inset-0 bg-gradient-to-b from-[#1a1a1a] to-[#1a1a1a] opacity-60 z-1"></div>
         <Header />
-        <motion.div
-          className="sticky top-0 pt-[10rem] sm:pr-[8rem] sm:pl-[8rem] md:pr-[13rem] md:pl-[13rem] lg:pr-[18rem] lg:pl-[18rem] xl:pr-[23rem] xl:pl-[23rem] 2xl:pr-[27rem] 2xl:pl-[27rem]"
-          style={useElementOneTransform()}
-        >
-          <HeroSection />
-        </motion.div>
-
-        <div className="pt-[12rem] relative w-full flex flex-col justify-center sm:pr-20 md:pr-40 lg:pr-60 xl:pr-80 2xl:pr-96 sm:pl-20 md:pl-40 lg:pl-60 xl:pl-80 2xl:pl-96"></div>
+        <HeroSection />
         <About />
         <TagLine />
         <Experience />
@@ -36,13 +29,6 @@ export default function Homepage() {
       </ReactLenis>
     </div>
   )
-}
-
-const useElementOneTransform = () => {
-  const { scrollY } = useScroll()
-  const opacity = useTransform(scrollY, [0, 80, 350], [1, 1, 0])
-  const scale = useTransform(scrollY, [0, 40, 450], [1, 1, 0.7])
-  return { opacity, scale }
 }
 
 const useVideoTransform = () => {
