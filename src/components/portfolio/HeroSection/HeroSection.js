da671b1afc7b45ee4cac418138b1893c5b8ffd0c
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-10-09 21:51:02 -0400

    logo resizing

--- src/components/portfolio/HeroSection/HeroSection.js
+++ src/components/portfolio/HeroSection/HeroSection.js
@@ -5,13 +5,11 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 export default function HeroSection() {
   return (
     <motion.div
-      className="sticky top-0 pt-[10rem] sm:pr-[8rem] sm:pl-[8rem] md:pr-[13rem] md:pl-[13rem] lg:pr-[18rem] lg:pl-[18rem] xl:pr-[23rem] xl:pl-[23rem] 2xl:pr-[27rem] 2xl:pl-[27rem]"
+      className="sticky top-0 pt-[2rem] sm:pr-[8rem] sm:pl-[8rem] md:pr-[13rem] md:pl-[13rem] lg:pr-[18rem] lg:pl-[18rem] xl:pr-[23rem] xl:pl-[23rem] 2xl:pr-[27rem] 2xl:pl-[27rem]"
       style={useElementOneTransform()}
     >
-      <div className="flex-1 z-10 pl-12 pr-12 flex justify-center">
-        <div className="lg:pl-16 lg:pr-16 1xl:pl-24 1xl:pr-24 2xl:pl-36 2xl:pr-36">
-          <motion.img className="rounded-3xl " src={AlexLogo} alt="Alexandra Biehle" style={{ willChange: 'opacity, transform', useElementOneTransform }} />
-        </div>
+      <div className="lg:pl-16 lg:pr-16 1xl:pl-24 1xl:pr-24 2xl:pl-36 2xl:pr-36">
+        <motion.img initial={{ y: -400 }} animate={{ y: 0 }} transition={{ duration: 0.8, type: 'spring' }} src={AlexLogo} alt="Alexandra Biehle" />
       </div>
     </motion.div>
   )
