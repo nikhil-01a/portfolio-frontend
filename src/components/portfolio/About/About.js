fb9f0981ee3e7a48922136b3ebf821b5477fb099
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-10-12 00:09:15 -0400

    finished content in about me

--- src/components/portfolio/About/About.js
+++ src/components/portfolio/About/About.js
@@ -2,9 +2,9 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function About() {
   const { scrollY } = useScroll()
-  const y = useTransform(scrollY, [300, 850], [-250, 0])
-  const scale = useTransform(scrollY, [850, 900], [0.9, 1])
-  const opacity = useTransform(scrollY, [500, 800, 850, 1250, 1400], [0, 0.3, 1, 1, 0])
+  const y = useTransform(scrollY, [300, 870], [-250, 0])
+  const scale = useTransform(scrollY, [870, 900], [0.9, 1])
+  const opacity = useTransform(scrollY, [500, 800, 870, 1250, 1400], [0, 0.3, 1, 1, 0])
 
   return (
     <div>
@@ -12,11 +12,14 @@ export default function About() {
         <motion.h1 className="text-[3rem] 1xl:text-[3.5rem]" style={{ y, scale, opacity }}>
           ABOUT ME
         </motion.h1>
-        <motion.p className="content-text pt-[1rem] text-[1.1rem] 1xl:text-[1.7rem]" style={useContentTransform()}>
-          My expertise spans Packaging Design, Graphic Design, Advertising & Digital Design, and Spatial Experience Design, with a strong foundation in Adobe Creative Suite.
-          <br />
-          <br />I value attention to detail, composition, and color, always approaching each project with meticulous care. My dream is to collaborate with others on projects that make an impact,
-          inspire, and bring joy.
+        <motion.p className="content-text pt-[1.1rem] text-[1.1rem] 1xl:text-[1.7rem]" style={useContentTransform()}>
+          <ul>
+            <li> My expertise spans Packaging Design, Graphic Design, Advertising & Digital Design, and Spatial Experience Design, with a strong foundation in Adobe Creative Suite.</li>
+            <li className="pt-[1.5rem]">
+              I value attention to detail, composition, and color, always approaching each project with meticulous care. My dream is to collaborate with others on projects that make an impact,
+              inspire, and bring joy.
+            </li>
+          </ul>
         </motion.p>
       </div>
     </div>
@@ -25,8 +28,8 @@ export default function About() {
 
 const useContentTransform = () => {
   const { scrollY } = useScroll()
-  const y = useTransform(scrollY, [300, 850], [600, 0])
-  const scale = useTransform(scrollY, [850, 900], [0.9, 1])
-  const opacity = useTransform(scrollY, [500, 800, 850, 1250, 1400], [0, 0.3, 1, 1, 0])
+  const y = useTransform(scrollY, [300, 870], [600, 0])
+  const scale = useTransform(scrollY, [870, 900], [0.9, 1])
+  const opacity = useTransform(scrollY, [500, 800, 870, 1250, 1400], [0, 0.3, 1, 1, 0])
   return { scale, opacity, y }
 }
