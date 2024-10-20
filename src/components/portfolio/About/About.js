63cfbefbc377fe416de704d06483dfc8bf2d6553
Nikhil Kumar Goutham Kumar <nkumarg@stevens.edu>
2024-10-20 02:44:12 -0400

    fixed bugs

--- src/components/portfolio/About/About.js
+++ src/components/portfolio/About/About.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react'
 
 export default function About() {
   const { scrollY } = useScroll()
-  const [screenHeight, setScreenHeight] = useState(0)
+  const [scHeight, setScHeight] = useState(0)
   const [visible, setVisible] = useState(true)
 
   // Effect to get the current screen height
   useEffect(() => {
-    const handleResize = () => setScreenHeight(window.innerHeight)
+    const handleResize = () => setScHeight(window.innerHeight)
     handleResize() // Set initial height
     window.addEventListener('resize', handleResize)
 
@@ -16,17 +16,13 @@ export default function About() {
   }, [])
 
   // Calculate transforms based on the screen height (responsive)
-  const y = useTransform(scrollY, [screenHeight * 0.4, screenHeight * 0.6, screenHeight * 1.4, screenHeight * 1.45], [600, 0, 0, -600])
-  const scale = useTransform(scrollY, [screenHeight * 0.9, screenHeight * 0.95, screenHeight * 1.3, screenHeight * 1.35], [0.9, 1, 1, 0.9])
-  const opacity = useTransform(
-    scrollY,
-    [screenHeight * 0.6, screenHeight * 0.9, screenHeight * 0.95, screenHeight * 1.2, screenHeight * 1.3, screenHeight * 1.35, screenHeight * 1.6],
-    [0.3, 0.3, 1, 1, 1, 0.3, 0]
-  )
+  const y = useTransform(scrollY, [scHeight * 0.4, scHeight * 0.6, scHeight * 1.4, scHeight * 1.45], [600, 0, 0, -600])
+  const scale = useTransform(scrollY, [scHeight * 0.9, scHeight * 0.95, scHeight * 1.3, scHeight * 1.35], [0.9, 1, 1, 0.9])
+  const opacity = useTransform(scrollY, [scHeight * 0.6, scHeight * 0.9, scHeight * 0.95, scHeight * 1.2, scHeight * 1.3, scHeight * 1.35, scHeight * 1.6], [0.3, 0.3, 1, 1, 1, 0.3, 0])
 
   useMotionValueEvent(scrollY, 'change', (latest) => {
-    if (latest < screenHeight * 1.6 && !visible) setVisible(true)
-    else if (latest >= screenHeight * 1.6 && visible) setVisible(false)
+    if (latest < scHeight * 1.6 && !visible) setVisible(true)
+    else if (latest >= scHeight * 1.6 && visible) setVisible(false)
   })
 
   return (
@@ -34,7 +30,7 @@ export default function About() {
       <motion.h1 className="hero-text text-[3rem] 1xl:text-[3.5rem]" style={{ y, scale, opacity }}>
         ABOUT ME
       </motion.h1>
-      <motion.p className="content-text pt-[1.1rem] text-3xl max-w-[75rem] p-10" style={useContentTransform(screenHeight)}>
+      <motion.p className="content-text pt-[1.1rem] text-3xl max-w-[75rem] p-10" style={useContentTransform(scHeight)}>
         <ul>
           <li> My expertise spans Packaging Design, Graphic Design, Advertising & Digital Design, and Spatial Experience Design, with a strong foundation in Adobe Creative Suite.</li>
           <li className="pt-[1.5rem]">
@@ -48,16 +44,12 @@ export default function About() {
 }
 
 // Define content transform for responsive animations
-const useContentTransform = (screenHeight) => {
+const useContentTransform = (scHeight) => {
   const { scrollY } = useScroll()
 
   // Use relative values (based on screen height) for responsive transforms
-  const y = useTransform(scrollY, [screenHeight * 0.6, screenHeight * 0.85], [600, 0])
-  const scale = useTransform(scrollY, [screenHeight * 0.9, screenHeight * 0.95, screenHeight * 1.3, screenHeight * 1.35], [0.9, 1, 1, 0.9])
-  const opacity = useTransform(
-    scrollY,
-    [screenHeight * 0.6, screenHeight * 0.9, screenHeight * 0.95, screenHeight * 1.2, screenHeight * 1.3, screenHeight * 1.35, screenHeight * 1.6],
-    [0.3, 0.3, 1, 1, 1, 0.3, 0]
-  )
+  const y = useTransform(scrollY, [scHeight * 0.6, scHeight * 0.85], [600, 0])
+  const scale = useTransform(scrollY, [scHeight * 0.9, scHeight * 0.95, scHeight * 1.3, scHeight * 1.35], [0.9, 1, 1, 0.9])
+  const opacity = useTransform(scrollY, [scHeight * 0.6, scHeight * 0.9, scHeight * 0.95, scHeight * 1.2, scHeight * 1.3, scHeight * 1.35, scHeight * 1.6], [0.3, 0.3, 1, 1, 1, 0.3, 0])
   return { scale, opacity, y }
 }
--- src/components/portfolio/Experience/Experience.js
+++ src/components/portfolio/Experience/Experience.js
@@ -1,13 +1,28 @@
 import { motion, useScroll, useTransform } from 'framer-motion'
+import { useState, useEffect } from 'react'
 export default function Experience() {
   const { scrollY } = useScroll()
-  const opacity = useTransform(scrollY, [2000, 2000], [0, 1])
+  const [scHeight, setScHeight] = useState(0)
+
+  useEffect(() => {
+    const handleSize = () => {
+      setScHeight(window.innerHeight)
+    }
+    handleSize()
+
+    window.addEventListener('resize', handleSize)
+
+    return () => {
+      window.removeEventListener('resize', handleSize)
+    }
+  }, [])
+
   const experienceOpacity = useTransform(scrollY, [2550, 2550, 2900, 2950], [0, 0.2, 0.2, 1])
   const experienceScale = useTransform(scrollY, [2550, 2550, 2900, 2950], [0.98, 0.98, 0.98, 1])
 
   return (
     <div className=" flex flex-col pt-[52rem]  sm:pr-20 md:pr-40 lg:pr-60 xl:pr-48 sm:pl-20 md:pl-20 lg:pl-40 xl:pl-48 hero-text">
-      <motion.div className="flex-col items-start" style={{ opacity }}>
+      <motion.div className="flex-col items-start">
         <motion.h1 className="text-[3rem] 1xl:text-[3.5rem] leading-[2.5rem] 1xl:leading-[3rem]">RELEVANT</motion.h1>
         <motion.h1 className="text-[3rem] 1xl:text-[3.5rem] leading-[2.5rem] 1xl:leading-[3rem]">EXPERIENCE</motion.h1>
       </motion.div>
